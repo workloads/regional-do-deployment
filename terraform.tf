efd112cc9dba4191f149cd2f585532b2ef7f30ad
Kerim Satirli <k.satirli@gmail.com>
2024-02-24 14:10:15 -0500

    bumps providers

--- terraform.tf
+++ terraform.tf
@@ -6,13 +6,13 @@ terraform {
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/digitalocean/digitalocean/2.32.0/
+    # see https://registry.terraform.io/providers/digitalocean/digitalocean/2.34.1/
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 2.32.0, < 3.0.0"
+      version = ">= 2.34.1, < 3.0.0"
     }
   }
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
-  required_version = ">= 1.6.0, < 2.0.0"
+  required_version = ">= 1.7.0, < 2.0.0"
 }
